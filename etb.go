3c94417680c0b71c70735a843bd1f33a03c82122
earentir <earentir@gmail.com>
2022-02-01 20:14:54 +0200

    new struct for credentials (need to move from old sub LoadJSONTOStruct used to load any json to a struct (intf{})

--- etb.go
+++ etb.go
@@ -1,10 +1,26 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"time"
 )
 
+type Credentials struct {
+	TwitchPassword    string `json:"twitch_password,omitempty"`
+	TwitchClientID    string `json:"twitch_client_id,omitempty"`
+	OpenWeatherAPIKey string `json:"openweathermapapi,omitempty"`
+}
+
+var creds Credentials
+
+var settings Settings
+
 func main() {
+
+	LoadJSONTOStruct("etb-settings.json", &settings)
+
 	etb := BasicBot{
 		Channel:     "earentir",
 		Name:        "duotronics",
@@ -14,5 +30,19 @@ func main() {
 		MsgRate:     time.Duration(4000),
 	}
 
+	LoadJSONTOStruct(etb.PrivatePath, &creds)
+
+	fmt.Println(etb.PrivatePath)
+	fmt.Println(creds.OpenWeatherAPIKey)
+
 	etb.Start()
+}
+
+func LoadJSONTOStruct(jsonFileName string, onTo interface{}) {
+	//read json here
+	jsonFile, err := ioutil.ReadFile(jsonFileName)
+	if nil != err {
+		fmt.Println(err)
+	}
+	json.Unmarshal([]byte(jsonFile), &onTo)
 }
