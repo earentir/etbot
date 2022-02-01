f14b89e4ed45430df705b69c8cad799f5e8a712c
earentir <earentir@gmail.com>
2022-02-01 20:11:15 +0200

    moved command parsing to new module

--- bot.go
+++ bot.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -10,7 +9,6 @@ import (
 	"io/ioutil"
 	"net"
 	"net/textproto"
-	"os/exec"
 	"regexp"
 	"strings"
 	"time"
@@ -38,16 +36,12 @@ var MsgRegex *regexp.Regexp = regexp.MustCompile(`^:(\w+)!\w+@\w+\.tmi\.twitch\.
 var CmdRegex *regexp.Regexp = regexp.MustCompile(`^!(\w+)\s?(\w+)?`)
 
 type OAuthCred struct {
-
-	// The bot account's OAuth password.
-	Password string `json:"password,omitempty"`
-
-	// The developer application client ID. Used for API calls to Twitch.
-	ClientID string `json:"client_id,omitempty"`
+	TwitchPassword    string `json:"twitch_password,omitempty"`
+	TwitchClientID    string `json:"twitch_client_id,omitempty"`
+	OpenWeatherAPIKey string `json:"openweathermapapi,omitempty"`
 }
 
 type Bot interface {
-
 	// Opens a connection to the Twitch.tv IRC chat server.
 	Connect()
 
@@ -140,14 +134,11 @@ func (bb *BasicBot) HandleChat() error {
 	// listens for chat messages
 	for {
 		line, err := tp.ReadLine()
-
-		rawLine := line
+		// rawLine := line
 
 		if nil != err {
-
 			// officially disconnects the bot from the server
 			bb.Disconnect()
-
 			return errors.New("bb.Bot.HandleChat: Failed to read line from channel. Disconnected")
 		}
 
@@ -164,133 +155,12 @@ func (bb *BasicBot) HandleChat() error {
 			if nil != matches {
 				userName := matches[1]
 				msgType := matches[2]
+				msg := matches[3]
 
-				switch msgType {
-				case "PRIVMSG":
-					msg := matches[3]
-					rgb.GPrintf("[%s] %s: %s\n", timeStamp(), userName, msg)
+				ParseCommand(bb, msgType, msg, userName)
 
-					// parse commands from user message
-					cmdMatches := CmdRegex.FindStringSubmatch(msg)
-					if nil != cmdMatches {
-						cmd := cmdMatches[1]
-
-						switch cmd {
-						case "etbdown":
-							if inArray(AdminUsers, userName) {
-								rgb.CPrintf(
-									"[%s] Shutdown command received. Shutting down now...\n",
-									timeStamp(),
-								)
-
-								bb.Disconnect()
-								// return nil
-							}
-
-						case "hi":
-							atIndex := strings.Index(msg, "@")
-							if atIndex > -1 {
-								atUser := getAttributedUser(msg, true)
-								bb.Say(fmt.Sprintf("earentHey %s, @%s says Hi!", atUser, userName))
-							} else {
-								bb.Say(fmt.Sprintf("earentHey @%s", userName))
-							}
-
-						case "oil":
-							bb.Say(oliveoil() + " " + getAttributedUser(msg, true))
-
-						case "bofh":
-							bb.Say(JokesBOFH(HTTPGetBody("http://api.esgr.xyz/fun.json/jokes/bofh")) + " " + getAttributedUser(msg, true))
-
-						case "joke":
-							fallthrough
-						case "yoke":
-							bb.Say(JokesJoke(HTTPGetBody("http://api.esgr.xyz/fun.json/jokes/joke")) + " " + getAttributedUser(msg, true))
-
-						case "lurk":
-							if msg == "!"+cmd {
-								bb.Say(fmt.Sprintf("Thank you for lurking %s, you smart hooman, go have as much fun as possible on your endeavours", userName))
-							} else {
-								bb.Say(fmt.Sprintf("Thank you for lurking %s, you smart hooman, go have fun with %s", userName, getCleanMessage(cmd, msg)))
-							}
-
-						case "socials":
-							fallthrough
-						case "discord":
-							bb.Say("no")
-
-						case "hype":
-							atUser := getAttributedUser(msg, true)
-							if atUser != "" {
-								bb.Say(fmt.Sprintf("earentFfs %s, dont you think there is better places to spend your money ? Stop wasting it !!!", atUser))
-							} else {
-								bb.Say("Please @ a user")
-							}
-
-						case "w":
-							fallthrough
-						case "weather":
-							if msg == "!"+cmd {
-								bb.Say(getWeather("Athens, Greece"))
-							} else {
-								bb.Say(getWeather(getCleanMessage(cmd, msg)) + " " + getAttributedUser(msg, true))
-							}
-
-						case "pro": //check if they stream and say pro streamer otherwise pro viewer
-							bb.Say("soon")
-
-						case "time":
-							bb.Say(timeStamp())
-
-						case "commands":
-							bb.Say("Available Commands: hi, so, bofh, joke, oil, weather, hype, lurk, time, fr")
-
-						case "so":
-							if inArray(AdminUsers, userName) || inArray(MODUsers, userName) || inArray(VIPUsers, userName) {
-								var souser string
-
-								if msg != "!so" {
-									if msg[4:5] == "@" {
-										souser = msg[5:]
-									} else {
-										souser = msg[4:]
-									}
-								} else {
-									bb.Say(fmt.Sprintf("@%s Please use !so @username", userName))
-								}
-
-								bb.Say(fmt.Sprintf("Please check out & follow %s @ https://twitch.tv/%s they are amazing.%s", souser, souser, getItchIOProfile(souser)))
-							}
-
-						case "fr":
-							if inArray(AdminUsers, userName) || inArray(MODUsers, userName) || inArray(VIPUsers, userName) {
-
-								if len(getCleanMessage(cmd, msg)) > 1 {
-
-									title := getCleanMessage(cmd, msg)[1:]
-									cmdres := exec.Command("gh", "issue", "create", fmt.Sprintf("-t %s from %s", title, userName), "-b \"\" ", "-lchat-bot")
-
-									var errbuf bytes.Buffer
-									cmdres.Stderr = &errbuf
-
-									cmdres.Run()
-									if len(errbuf.String()) == 0 {
-										bb.Say("Feature Request Issue Opened")
-									}
-									fmt.Println(errbuf.String())
-								}
-							}
-
-						default:
-							// do nothing
-							// }
-						}
-					}
-				default:
-					// fmt.Printf("no priv: %s", line)
-				}
 			} else {
-				fmt.Println(rawLine)
+				// fmt.Println(rawLine)
 			}
 			time.Sleep(bb.MsgRate)
 		}
@@ -300,7 +170,7 @@ func (bb *BasicBot) HandleChat() error {
 // Makes the bot join its pre-specified channel.
 func (bb *BasicBot) JoinChannel() {
 	rgb.YPrintf("[%s] Joining #%s...\n", timeStamp(), bb.Channel)
-	bb.conn.Write([]byte("PASS " + bb.Credentials.Password + "\r\n"))
+	bb.conn.Write([]byte("PASS " + bb.Credentials.TwitchPassword + "\r\n"))
 	bb.conn.Write([]byte("NICK " + bb.Name + "\r\n"))
 	bb.conn.Write([]byte("JOIN #" + bb.Channel + "\r\n"))
 
