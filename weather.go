63e84e0f40735e2e14926951874f30310dabd58f
earentir <earentir@gmail.com>
2022-02-01 20:16:37 +0200

    we get the api key from the new cred store
    we check if we have an api key or its invalid and we print help

--- weather.go
+++ weather.go
@@ -49,21 +49,24 @@ type openweathermap struct {
 }
 
 func getWeather(city string) string {
-	weatherjson := HTTPGetBody(fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s&units=metric", city, readTextFile("owapikey.txt")[0]))
 
-	openweathermapdata := openweathermap{}
+	if creds.OpenWeatherAPIKey != "" {
+		weatherjson := HTTPGetBody(fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s&units=metric", city, creds.OpenWeatherAPIKey))
 
-	// fmt.Print("we: " + string(weatherjson))
+		openweathermapdata := openweathermap{}
 
-	if len(weatherjson) > 0 {
-		err := json.Unmarshal([]byte(weatherjson), &openweathermapdata)
-		if err != nil {
-			fmt.Print(err)
+		if len(weatherjson) > 0 {
+			err := json.Unmarshal([]byte(weatherjson), &openweathermapdata)
+			if err != nil {
+				fmt.Print(err)
+			}
+
+			// fmt.Println(openweathermapdata.Weather[0].Description)
+			return fmt.Sprintf("The temp in %s is %v° (feels like %v°) with %s", city, openweathermapdata.Main.Temp, openweathermapdata.Main.FeelsLike, openweathermapdata.Weather[0].Description)
 		}
 
-		// fmt.Println(openweathermapdata.Weather[0].Description)
-		return fmt.Sprintf("The temp in %s is %v° (feels like %v°) with %s", city, openweathermapdata.Main.Temp, openweathermapdata.Main.FeelsLike, openweathermapdata.Weather[0].Description)
+		return "Please add a correct City, Country combination, like  \"Athens, Greece\""
+	} else {
+		return "Please setup your openweathermap API key @ https://openweathermap.org"
 	}
-
-	return "Please add a correct City, Country combination, like  \"Athens, Greece\""
 }
