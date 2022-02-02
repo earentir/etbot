ce42bbc6a91cc268cc3111703f72275c11e81c52
earentir <earentir@gmail.com>
2022-02-02 17:03:01 +0200

    echange now has options

--- currency.go
+++ currency.go
@@ -9,24 +9,23 @@ func CurrencyConversion(ISOFROM, ISOTO string, amount float64) string {
 
 	buildPair := fmt.Sprintf("%s_%s", ISOFROM, ISOTO)
 
-	var pairValue map[string]interface{}
+	var pairValue map[string]float64
 
 	if creds.OpenWeatherAPIKey != "" {
 
 		exchangerate := HTTPGetBody(fmt.Sprintf("https://free.currconv.com/api/v7/convert?q=%s&compact=ultra&apiKey=%s", buildPair, creds.CurrencyAPIKey))
-
 		if len(exchangerate) > 0 {
 			err := json.Unmarshal([]byte(exchangerate), &pairValue)
 			if err != nil {
 				fmt.Print(err)
 			}
 
-			return fmt.Sprintf("The Exchange Rate of %s to %s is %v", ISOFROM, ISOTO, pairValue[buildPair])
+			return fmt.Sprintf("The Exchange Rate of %v %s to %s is %s", fmt.Sprintf("%.2f", amount), ISOFROM, ISOTO, fmt.Sprintf("%.2f", pairValue[buildPair]*amount))
 		}
 
-		return "Please add a correct City, Country combination, like  \"Athens, Greece\""
+		return "Please add the ISO codes of FIAT currencies"
 	} else {
-		return "Please setup your openweathermap API key @ https://openweathermap.org"
+		return "Please setup your Currency Conversion API key @ https://currconv.com"
 	}
 
 }
