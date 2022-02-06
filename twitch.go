efaa805465663364acf56b762aa794a41be5e16e
earentir <earentir@gmail.com>
2022-02-06 19:20:58 +0200

    add twitch integration

---
+++ twitch.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/nicklaw5/helix/v2"
+)
+
+func userExists(userName string) bool {
+	if len(getTwitchUser(userName)) > 0 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func getTwitchUser(userName string) TwitchUserData {
+	var twitchUserData TwitchUserData
+
+	client, err := helix.NewClient(&helix.Options{
+		ClientID:        creds.TwitchClientID,
+		UserAccessToken: creds.TwitchAppToken,
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	resp, err := client.GetUsers(&helix.UsersParams{
+		Logins: []string{userName},
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	userdata := &resp.Data.Users
+	jsonuserdata, err := json.Marshal(userdata)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	LoadJSONTOStruct(jsonuserdata, &twitchUserData)
+	return twitchUserData
+}
