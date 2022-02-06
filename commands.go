bbfc2ebfe55f6caf9a93018151aa7d2c6207812f
earentir <earentir@gmail.com>
2022-02-06 19:16:59 +0200

    ban/unban commands added

--- commands.go
+++ commands.go
@@ -6,139 +6,138 @@ import (
 	"os/exec"
 	"strconv"
 	"strings"
-
-	rgb "github.com/foresthoffman/rgblog"
 )
 
 func ParseCommand(bb *BasicBot, msgType, msg, userName string) {
 	switch msgType {
 	case "PRIVMSG":
 
-		rgb.GPrintf("[%s] %s: %s\n", timeStamp(), userName, msg)
+		CPrint("c", fmt.Sprintf("[%s] %s: %s\n", timeStamp(), userName, msg))
 
 		// parse commands from user message
 		cmdMatches := CmdRegex.FindStringSubmatch(msg)
 		if nil != cmdMatches {
 			cmd := cmdMatches[1]
-
-			switch cmd {
-			case "etbdown":
-				if inArray(AdminUsers, userName) {
-					rgb.CPrintf(
-						"[%s] Shutdown command received. Shutting down now...\n",
-						timeStamp(),
-					)
-
+			if CMDCanRun(userName, cmd) {
+				switch cmd {
+				case "etbdown":
+					CPrint("c", fmt.Sprintf("[%s] Shutdown command received. Shutting down now...\n", timeStamp()))
 					bb.Disconnect()
-					// return nil
-				}
 
-				//region joke commands
-			case "hi":
-				atIndex := strings.Index(msg, "@")
-				if atIndex > -1 {
+					//region joke commands
+				case "hi":
+					atIndex := strings.Index(msg, "@")
+					if atIndex > -1 {
+						atUser := getAttributedUser(msg, true)
+						bb.Say(fmt.Sprintf("earentHey %s, @%s says Hi!", atUser, userName))
+					} else {
+						bb.Say(fmt.Sprintf("earentHey @%s", userName))
+					}
+				case "olive":
+					fallthrough
+				case "oil":
+					bb.Say(oliveoil() + " " + getAttributedUser(msg, true))
+				case "yogurt":
+					bb.Say(yogurt() + " " + getAttributedUser(msg, true))
+				case "bofh":
+					bb.Say(JokesBOFH(HTTPGetBody("http://api.esgr.xyz/fun.json/jokes/bofh")) + " " + getAttributedUser(msg, true))
+				case "socials":
+					fallthrough
+				case "discord":
+					bb.Say("no")
+				case "sudo":
+					bb.Say("Fuck Off")
+				case "joke":
+					fallthrough
+				case "yoke":
+					bb.Say(JokesJoke(HTTPGetBody("http://api.esgr.xyz/fun.json/jokes/joke")) + " " + getAttributedUser(msg, true))
+				case "pro": //check if they stream and say pro streamer otherwise pro viewer
+					bb.Say("soon")
+				case "ban":
+					if msg == "!"+cmd {
+						bb.Say(fmt.Sprintf("%s banned for life, they suck.", userName))
+					} else {
+						bb.Say(fmt.Sprintf("%s banned for life, they suck.", getAttributedUser(msg, true)))
+					}
+				case "unban":
+					if msg == "!"+cmd || getAttributedUser(msg, true) == "" {
+						bb.Say(fmt.Sprintf("%s are unbanned, they dont suck anymore.", userName))
+					} else {
+						bb.Say(fmt.Sprintf("%s are unbanned, they dont suck anymore", getAttributedUser(msg, true)))
+					}
+					//endregion
+
+				case "lurk":
+					if msg == "!"+cmd {
+						bb.Say(fmt.Sprintf("Thank you for lurking %s, you smart hooman, go have as much fun as possible on your endeavours", userName))
+					} else {
+						bb.Say(fmt.Sprintf("Thank you for lurking %s, you smart hooman, go have fun with %s", userName, getCleanMessage(cmd, msg)))
+					}
+
+				case "hype":
 					atUser := getAttributedUser(msg, true)
-					bb.Say(fmt.Sprintf("earentHey %s, @%s says Hi!", atUser, userName))
-				} else {
-					bb.Say(fmt.Sprintf("earentHey @%s", userName))
-				}
-			case "olive":
-				fallthrough
-			case "oil":
-				bb.Say(oliveoil() + " " + getAttributedUser(msg, true))
-			case "yogurt":
-				bb.Say(yogurt() + " " + getAttributedUser(msg, true))
-			case "bofh":
-				bb.Say(JokesBOFH(HTTPGetBody("http://api.esgr.xyz/fun.json/jokes/bofh")) + " " + getAttributedUser(msg, true))
-			case "socials":
-				fallthrough
-			case "discord":
-				bb.Say("no")
-			case "sudo":
-				bb.Say("Fuck Off")
-			case "joke":
-				fallthrough
-			case "yoke":
-				bb.Say(JokesJoke(HTTPGetBody("http://api.esgr.xyz/fun.json/jokes/joke")) + " " + getAttributedUser(msg, true))
-			case "pro": //check if they stream and say pro streamer otherwise pro viewer
-				bb.Say("soon")
-				//endregion
-
-			case "lurk":
-				if msg == "!"+cmd {
-					bb.Say(fmt.Sprintf("Thank you for lurking %s, you smart hooman, go have as much fun as possible on your endeavours", userName))
-				} else {
-					bb.Say(fmt.Sprintf("Thank you for lurking %s, you smart hooman, go have fun with %s", userName, getCleanMessage(cmd, msg)))
-				}
-
-			case "hype":
-				atUser := getAttributedUser(msg, true)
-				if atUser != "" {
-					bb.Say(fmt.Sprintf("earentFfs %s, dont you think there is better places to spend your money ? Stop wasting it !!!", atUser))
-				} else {
-					bb.Say("Please @ a user")
-				}
-
-			case "ex":
-				fallthrough
-			case "exchange":
-				parts := strings.Fields(strings.ToUpper(msg))
-
-				switch len(parts) {
-				case 1:
-					bb.Say(CurrencyConversion(settings.General.Curency.DefaultCurrency, settings.General.Curency.CurrencyTo, 1))
-				case 2:
-					bb.Say(CurrencyConversion(settings.General.Curency.DefaultCurrency, parts[1], 1))
-				case 3:
-					_, err := strconv.Atoi(parts[2])
-					if err != nil {
-						bb.Say(CurrencyConversion(parts[1], parts[2], 1))
+					if atUser != "" {
+						bb.Say(fmt.Sprintf("earentFfs %s, dont you think there is better places to spend your money ? Stop wasting it !!!", atUser))
 					} else {
-						mnt, _ := strconv.ParseFloat(parts[2], 64)
-						bb.Say(CurrencyConversion(settings.General.Curency.DefaultCurrency, parts[1], mnt))
+						bb.Say("Please @ a user")
 					}
-				case 4:
-					mnt, _ := strconv.ParseFloat(parts[3], 64)
-					bb.Say(CurrencyConversion(parts[1], parts[2], mnt))
-				}
 
-			case "w":
-				fallthrough
-			case "weather":
-				if msg == "!"+cmd {
-					bb.Say(getWeather(settings.General.Weather.DefaultCity))
-				} else {
-					bb.Say(getWeather(getCleanMessage(cmd, msg)) + " " + getAttributedUser(msg, true))
-				}
+				case "ex":
+					fallthrough
+				case "exchange":
+					parts := strings.Fields(strings.ToUpper(msg))
 
-			case "time":
-				bb.Say(timeStamp())
-
-			case "commands":
-				bb.Say("Available Commands: hi, so, bofh, joke, oil, yogurt, weather, hype, lurk, time, fr, exchange")
-
-			case "so":
-				if inArray(AdminUsers, userName) || inArray(MODUsers, userName) || inArray(VIPUsers, userName) {
-					var souser string
-
-					if msg != "!so" {
-						if msg[4:5] == "@" {
-							souser = msg[5:]
+					switch len(parts) {
+					case 1:
+						bb.Say(CurrencyConversion(settings.General.Curency.DefaultCurrency, settings.General.Curency.CurrencyTo, 1))
+					case 2:
+						bb.Say(CurrencyConversion(settings.General.Curency.DefaultCurrency, parts[1], 1))
+					case 3:
+						_, err := strconv.Atoi(parts[2])
+						if err != nil {
+							bb.Say(CurrencyConversion(parts[1], parts[2], 1))
 						} else {
-							souser = msg[4:]
+							mnt, _ := strconv.ParseFloat(parts[2], 64)
+							bb.Say(CurrencyConversion(settings.General.Curency.DefaultCurrency, parts[1], mnt))
 						}
-					} else {
-						bb.Say(fmt.Sprintf("@%s Please use !so @username", userName))
+					case 4:
+						mnt, _ := strconv.ParseFloat(parts[3], 64)
+						bb.Say(CurrencyConversion(parts[1], parts[2], mnt))
 					}
 
-					bb.Say(fmt.Sprintf("Please check out & follow %s @ https://twitch.tv/%s they are amazing.%s", souser, strings.ToLower(souser), getItchIOProfile(souser)))
-				}
+				case "w":
+					fallthrough
+				case "weather":
+					if msg == "!"+cmd {
+						bb.Say(getWeather(settings.General.Weather.DefaultCity))
+					} else {
+						bb.Say(getWeather(getCleanMessage(cmd, msg)) + " " + getAttributedUser(msg, true))
+					}
 
-			case "fr":
-				if inArray(AdminUsers, userName) || inArray(MODUsers, userName) || inArray(VIPUsers, userName) || inArray(SubbedUsers, userName) {
+				case "time":
+					bb.Say(timeStamp())
+
+				case "commands":
+					bb.Say(fmt.Sprintf("Available Commands:  %s", getCMDS(userName)))
+
+				case "so":
+					var souser string
+					if userExists(userName) {
+						if msg != "!so" {
+							if msg[4:5] == "@" {
+								souser = msg[5:]
+							} else {
+								souser = msg[4:]
+							}
+						} else {
+							bb.Say(fmt.Sprintf("@%s Please use !so @username", userName))
+						}
+
+						bb.Say(fmt.Sprintf("Please check out & follow %s @ https://twitch.tv/%s they are amazing.%s", souser, strings.ToLower(souser), getItchIOProfile(souser)))
+					}
+				case "fr":
 
 					if len(getCleanMessage(cmd, msg)) > 1 {
-
 						title := getCleanMessage(cmd, msg)[1:]
 						cmdres := exec.Command("gh", "issue", "create", fmt.Sprintf("-t %s from %s", title, userName), "-b \"\" ", "-lchat-bot")
 
@@ -151,11 +150,11 @@ func ParseCommand(bb *BasicBot, msgType, msg, userName string) {
 						}
 						fmt.Println(errbuf.String())
 					}
-				}
 
-			default:
-				// do nothing
-				// }
+				default:
+					// do nothing
+					// }
+				}
 			}
 		}
 	default:
