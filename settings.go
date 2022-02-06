89e8ffc2456d50a778ef95a0b6671ae7bbea6150
earentir <earentir@gmail.com>
2022-02-06 19:19:19 +0200

    move structs to structs new cmd commands

--- settings.go
+++ settings.go
@@ -5,81 +5,42 @@ import (
 	"strings"
 )
 
-type Settings struct {
-	Acl struct {
-		Users []User `json:"users"`
-	} `json:"acl"`
-	Commands []Command  `json:"commands"`
-	Lurklist []LurkList `json:"lurklist"`
-	General  struct {
-		Servers struct {
-			Spotify struct {
-				Currentlyplaying bool `json:"currentlyplaying,omitempty"`
-			} `json:"spotify"`
-			Twitch struct {
-				Goals bool `json:"goals,omitempty"`
-			} `json:"twitch"`
-		} `json:"servers"`
-		Weather struct {
-			DefaultCity string `json:"default,omitempty"`
-		} `json:"weather"`
-		Curency struct {
-			DefaultCurrency string `json:"default,omitempty"`
-			CurrencyTo      string `json:"to,omitempty"`
-		} `json:"currency"`
-		UserLevels []UserLevelList `json:"userlevels"`
-	} `json:"general"`
+func getCMDS(userName string) string {
+	cmds := settings.Commands
+	var allcommands string = ""
+
+	for _, cm := range cmds {
+		if CMDCanRun(userName, cm.CommandName) {
+			allcommands = allcommands + ", " + cm.CommandName
+		}
+	}
+
+	return allcommands
 }
 
-type UserLevelList struct {
-	// Level []struct {
-	Level    int    `json:"level"`
-	Name     string `json:"name"`
-	Cooldown int    `json:"cooldown"`
-	// } `json:"userlevels"`
+func CMDCanRun(userName, cmd string) bool {
+	itcan := false
+
+	if CmdOpts(cmd).Enabled {
+		itcan = CmdOpts(cmd).UserLevel >= UserLevel(userName)
+	}
+
+	return itcan
 }
 
-type LurkList struct {
-	Lurkers []struct {
-		Lurker   string `json:"lurker"`
-		LurkedOn int64  `json:"lurkedon,omitempty"`
-	} `json:"lurklist"`
-}
-
-type User struct {
-	Name    string   `json:"name"`
-	Nick    string   `json:"nick,omitempty"`
-	Type    string   `json:"type"`
-	Socials []Social `json:"socials"`
-}
-
-type Social struct {
-	Twitch     string `json:"twitch,omitempty"`
-	Github     string `json:"github,omitempty"`
-	Itchio     string `json:"itchio,omitempty"`
-	Youtube    string `json:"youtube,omitempty"`
-	Twitter    string `json:"twiter,omitempty"`
-	Instagram  string `json:"instagram,omitempty"`
-	Facebook   string `json:"facebook,omitempty"`
-	Artstation string `json:"artstation,omitempty"`
-}
-
-type Command struct {
-	Commands []struct {
-		Msg                string `json:"msg,omitempty"`
-		Atmsg              string `json:"atmsg,omitempty"`
-		Help               string `json:"help,omitempty"`
-		Alias              string `json:"alias,omitempty"`
-		Lastuse            int    `json:"lastuse,omitempty"`
-		Counter            int    `json:"counter,omitempty"`
-		UserLevel          int    `json:"userlevel"`
-		CooldownMultiplier int    `json:"cooldownmultiplier"`
-		Enabled            bool   `json:"enabled,omitempty"`
-	} `json:"commands"`
+func CmdOpts(cmd string) CommandOption {
+	var commandOption CommandOption
+	cmds := settings.Commands
+	for _, cm := range cmds {
+		if (cmd == cm.CommandName) || (cmd == cm.CommandOptions.Alias) {
+			return cm.CommandOptions
+		}
+	}
+	return commandOption
 }
 
 func getUserSocials(userName string) []string {
-	setusers := settings.Acl.Users
+	setusers := settings.Users
 	found := []string{}
 
 	for _, usr := range setusers {
@@ -97,7 +58,7 @@ func getUserSocials(userName string) []string {
 func SearchUser(userName string) bool {
 	var found bool = false
 
-	setusers := settings.Acl.Users
+	setusers := settings.Users
 	for _, usr := range setusers {
 		if userName == usr.Name {
 			found = true
@@ -108,7 +69,7 @@ func SearchUser(userName string) bool {
 
 func convertLevelType(LevelType string) int {
 	var level int = 10
-	setusers := settings.General.UserLevels
+	setusers := settings.UserLevels
 	for _, lvl := range setusers {
 		if LevelType == lvl.Name {
 			level = lvl.Level
@@ -119,7 +80,7 @@ func convertLevelType(LevelType string) int {
 
 func getLevelCoolDown(level int) int {
 	var cooldown int = 30000
-	setusers := settings.General.UserLevels
+	setusers := settings.UserLevels
 	for _, lvl := range setusers {
 		if level == lvl.Level {
 			cooldown = lvl.Cooldown
@@ -130,7 +91,7 @@ func getLevelCoolDown(level int) int {
 
 func UserLevel(userName string) int {
 	var level int = 10
-	setusers := settings.Acl.Users
+	setusers := settings.Users
 	for _, usr := range setusers {
 		if userName == usr.Name {
 			level = convertLevelType(usr.Type)
