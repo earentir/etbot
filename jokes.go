a5f18dc7ee94a3fdae750d14493b460f2f8da28c
earentir <earentir@gmail.com>
2022-02-12 20:00:49 +0200

    rewrore joke api calls to make them work for both of our api calls

--- jokes.go
+++ jokes.go
@@ -7,45 +7,28 @@ import (
 	"time"
 )
 
-type bofh struct {
+type jokes struct {
 	JOKE struct {
-		BOFHline string `json:"bofhline"`
+		BOFHline string `json:"bofhline,omitempty"`
+		Q        string `json:"q,omitempty"`
+		A        string `json:"a,omitempty"`
 	} `json:"joke"`
 }
 
-type joke struct {
-	JOKE struct {
-		Q string `json:"q"`
-		A string `json:"a"`
-	} `json:"joke"`
-}
-
-func JokesBOFH(msg string) string {
-
-	bofhjson := bofh{}
-
-	err := json.Unmarshal([]byte(msg), &bofhjson)
-	if err != nil {
-		fmt.Print(err)
-	}
-
-	return bofhjson.JOKE.BOFHline
-}
-
-func JokesJoke(msg string) string {
-
-	jokejson := joke{}
+func JokesAPI(msg string) []string {
+	jokejson := jokes{}
+	var retuarr []string
 
 	err := json.Unmarshal([]byte(msg), &jokejson)
-	if err != nil {
+	if err == nil {
 		fmt.Print(err)
 	}
 
-	if jokejson.JOKE.A != "0" {
-		return jokejson.JOKE.Q + " " + jokejson.JOKE.A
-	} else {
-		return jokejson.JOKE.Q
-	}
+	retuarr = append(retuarr, jokejson.JOKE.BOFHline)
+	retuarr = append(retuarr, jokejson.JOKE.Q)
+	retuarr = append(retuarr, jokejson.JOKE.A)
+
+	return retuarr
 }
 
 func oliveoil() string {
