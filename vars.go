5d283a693f645df25b2ccf68fd787b243aa76d1a
earentir <earentir@gmail.com>
2022-02-12 20:13:23 +0200

    all vars are not here

---
+++ vars.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"regexp"
+	"time"
+)
+
+const (
+	UTCFormat = time.RFC3339
+)
+
+var (
+	// Regex for parsing PRIVMSG strings.
+	// First matched group is the user's name and the second matched group is the content of the
+	// user's message.
+	MsgRegex *regexp.Regexp = regexp.MustCompile(`^:(\w+)!\w+@\w+\.tmi\.twitch\.tv (PRIVMSG) #\w+(?: :(.*))?$`)
+
+	// Regex for parsing user commands, from already parsed PRIVMSG strings.
+	// First matched group is the command name and the second matched group is the argument for the
+	// command.
+	CmdRegex *regexp.Regexp = regexp.MustCompile(`^!(\w+)\s?(\w+)?`)
+)
