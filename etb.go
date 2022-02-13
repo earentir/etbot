426a7bd127e7f17edc5546e0d15cc6f1d5b626e3
earentir <earentir@gmail.com>
2022-02-13 20:23:16 +0200

    clean up

--- etb.go
+++ etb.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"time"
 )
 
@@ -22,45 +20,9 @@ func main() {
 
 	LoadJSONFileTOStruct(etb.PrivatePath, &creds)
 
-	//We can do stuff after
-
-	// fmt.Println(SearchUser("mrpewpewlaser"))
-	// fmt.Println(UserLevel("mrpewpewlaser"))
-	// fmt.Println(getLevelCoolDown(UserLevel("mrpewpewlaser")))
-
-	// fmt.Println(getUserSocials("sireeeki"))
-
-	// for _, soc := range getUserSocials("earentir") {
-	// 	fmt.Println(soc)
-	// }
-
-	// fmt.Println(CMDCanRun("wulgaru", "fr"))
-
-	// if userExists("earentir") {
-	// 	twusr := getTwitchUser("earentir")
-	// 	fmt.Println(twusr[0].ID)
-	// 	fmt.Println(twusr[0].Description)
-	// 	fmt.Println(twusr[0].ViewCount)
-	// 	fmt.Println(twusr[0].CreatedAt)
-	// }
-
-	// fmt.Println(getUserSocials("earentir"))
+	fmt.Println(jokesJSON("oil"))
 
 	if settings.Servers.BotServers.Chat {
 		etb.Start()
 	}
-}
-
-func LoadJSONFileTOStruct(jsonFileName string, onTo interface{}) {
-	//read json here
-	jsonFile, err := ioutil.ReadFile(jsonFileName)
-	if nil != err {
-		fmt.Println(err)
-	}
-	json.Unmarshal([]byte(jsonFile), &onTo)
-}
-
-func LoadJSONTOStruct(jsondata []byte, onTo interface{}) {
-	//read json here
-	json.Unmarshal(jsondata, &onTo)
 }
