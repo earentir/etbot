6c8444fcfa3e576ff9a129988130aa7307153d5b
earentir <earentir@gmail.com>
2022-02-16 10:01:03 +0200

    add def for file io we monitor sig for hup and if we catch we cleanup (save etc)

--- etb.go
+++ etb.go
@@ -1,25 +1,42 @@
 package main
 
 import (
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
 func main() {
 
-	LoadJSONFileTOStruct("etb-settings.json", &settings)
+	if _, err := os.Stat("etb-settings.json"); err == nil {
 
-	etb := BasicBot{
-		Channel:     settings.General.Twitch.Channel,
-		Name:        settings.General.Twitch.BotUserName,
-		Port:        settings.General.Twitch.IRCPort,
-		PrivatePath: settings.General.CredentialFile,
-		Server:      settings.General.Twitch.Server,
-		MsgRate:     time.Duration(settings.General.Twitch.MSGRate),
-	}
+		LoadJSONFileTOStruct("etb-settings.json", &settings)
 
-	LoadJSONFileTOStruct(etb.PrivatePath, &creds)
+		etb := BasicBot{
+			Channel:     settings.General.Twitch.Channel,
+			Name:        settings.General.Twitch.BotUserName,
+			Port:        settings.General.Twitch.IRCPort,
+			PrivatePath: settings.General.CredentialFile,
+			Server:      settings.General.Twitch.Server,
+			MsgRate:     time.Duration(settings.General.Twitch.MSGRate),
+		}
 
-	if settings.Servers.BotServers.Chat {
-		etb.Start()
+		if _, err := os.Stat(etb.PrivatePath); err == nil {
+			LoadJSONFileTOStruct(etb.PrivatePath, &creds)
+
+			//ffs we catch oob interupt, cause he keeps ctrl+c
+			c := make(chan os.Signal)
+			signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+			go func() {
+				<-c
+				cleanup()
+				os.Exit(1)
+			}()
+
+			if settings.Servers.BotServers.Chat {
+				etb.Start()
+			}
+		}
 	}
 }
