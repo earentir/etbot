36b31477c316bcd4e31e6f942b34e5685ca1aa5b
earentir <earentir@gmail.com>
2022-02-24 14:09:51 +0200

    move struct to proper file

--- bot.go
+++ bot.go
@@ -32,38 +32,6 @@ type Bot interface {
 	Start()
 }
 
-type BasicBot struct {
-
-	// The channel that the bot is supposed to join. Note: The name MUST be lowercase, regardless
-	// of how the username is displayed on Twitch.tv.
-	Channel string
-
-	// A reference to the bot's connection to the server.
-	conn net.Conn
-
-	// A forced delay between bot responses. This prevents the bot from breaking the message limit
-	// rules. A 20/30 millisecond delay is enough for a non-modded bot. If you decrease the delay
-	// make sure you're still within the limit!
-	//
-	// Message Rate Guidelines: https://dev.twitch.tv/docs/irc#irc-command-and-message-limits
-	MsgRate time.Duration
-
-	// The name that the bot will use in the chat that it's attempting to join.
-	Name string
-
-	// The port of the IRC server.
-	Port string
-
-	// A path to a limited-access directory containing the bot's OAuth credentials.
-	PrivatePath string
-
-	// The domain of the IRC server.
-	Server string
-
-	// The time at which the bot achieved a connection to the server.
-	startTime time.Time
-}
-
 // Connects the bot to the Twitch IRC server. The bot will continue to try to connect until it
 // succeeds or is forcefully shutdown.
 func (bb *BasicBot) Connect() {
--- structs.go
+++ structs.go
@@ -1,9 +1,41 @@
 package main
 
-import "time"
+import (
+	"net"
+	"time"
+)
 
-var creds Credentials
-var settings Settings
+type BasicBot struct {
+
+	// The channel that the bot is supposed to join. Note: The name MUST be lowercase, regardless
+	// of how the username is displayed on Twitch.tv.
+	Channel string
+
+	// A reference to the bot's connection to the server.
+	conn net.Conn
+
+	// A forced delay between bot responses. This prevents the bot from breaking the message limit
+	// rules. A 20/30 millisecond delay is enough for a non-modded bot. If you decrease the delay
+	// make sure you're still within the limit!
+	//
+	// Message Rate Guidelines: https://dev.twitch.tv/docs/irc#irc-command-and-message-limits
+	MsgRate time.Duration
+
+	// The name that the bot will use in the chat that it's attempting to join.
+	Name string
+
+	// The port of the IRC server.
+	Port string
+
+	// A path to a limited-access directory containing the bot's OAuth credentials.
+	PrivatePath string
+
+	// The domain of the IRC server.
+	Server string
+
+	// The time at which the bot achieved a connection to the server.
+	startTime time.Time
+}
 
 //Credentials struct
 type Credentials struct {
