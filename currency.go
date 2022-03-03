838b92c054d3cb8b452afaaa82ba0fd43780141e
earentir <earentir@gmail.com>
2022-03-03 20:50:14 +0200

    fixed #82

--- currency.go
+++ currency.go
@@ -14,7 +14,7 @@ func CurrencyConversion(ISOFROM, ISOTO string, amount float64) string {
 	if creds.OpenWeatherAPIKey != "" {
 
 		exchangerate := HTTPGetBody(fmt.Sprintf("https://free.currconv.com/api/v7/convert?q=%s&compact=ultra&apiKey=%s", buildPair, creds.CurrencyAPIKey))
-		if len(exchangerate) > 0 {
+		if len(exchangerate) > 0 && exchangerate != "{}" {
 			err := json.Unmarshal([]byte(exchangerate), &pairValue)
 			if err != nil {
 				fmt.Print(err)
@@ -22,7 +22,6 @@ func CurrencyConversion(ISOFROM, ISOTO string, amount float64) string {
 
 			return fmt.Sprintf("The Exchange Rate of %v %s to %s is %s", fmt.Sprintf("%.2f", amount), ISOFROM, ISOTO, fmt.Sprintf("%.2f", pairValue[buildPair]*amount))
 		}
-
 		return "Please add the ISO codes of FIAT currencies"
 	} else {
 		return "Please setup your Currency Conversion API key @ https://currconv.com"
