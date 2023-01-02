80731c732f5e312a0a834e96a67fbb26449f9143
earentir <earentir@gmail.com>
2023-01-02 17:13:41 +0200

    added return of error code in case we get an http error

--- currency.go
+++ currency.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
 )
 
 func CurrencyConversion(ISOFROM, ISOTO string, amount float64) string {
@@ -14,14 +15,19 @@ func CurrencyConversion(ISOFROM, ISOTO string, amount float64) string {
 	if creds.CurrencyAPIKey != "" {
 
 		exchangerate := HTTPGetBody(fmt.Sprintf("https://free.currconv.com/api/v7/convert?q=%s&compact=ultra&apiKey=%s", buildPair, creds.CurrencyAPIKey))
+		if strings.HasPrefix(exchangerate, "http/error/") {
+			return fmt.Sprintf("Currency Exchange Error: %s", exchangerate)
+		}
+
 		if len(exchangerate) > 0 && exchangerate != "{}" {
 			err := json.Unmarshal([]byte(exchangerate), &pairValue)
 			if err != nil {
-				fmt.Println(err)
+				fmt.Println("Currency Exchange Error: ", err)
 			}
 
 			return fmt.Sprintf("The Exchange Rate of %v %s to %s is %s", fmt.Sprintf("%.2f", amount), ISOFROM, ISOTO, fmt.Sprintf("%.2f", pairValue[buildPair]*amount))
 		}
+
 		return "Please add the ISO codes of FIAT currencies"
 	} else {
 		return "Please setup your Currency Conversion API key @ https://currconv.com"
