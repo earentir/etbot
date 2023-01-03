6d3fa5da6e26e3789d00a1bc205a6055d53edda7
earentir <earentir@gmail.com>
2023-01-03 12:25:04 +0200

    check for error

--- weather.go
+++ weather.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
 )
 
 func getWeather(city string) string {
@@ -11,6 +12,10 @@ func getWeather(city string) string {
 		weatherjson := HTTPGetBody(fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s&units=metric", city, creds.OpenWeatherAPIKey))
 
 		openweathermapdata := openweathermap{}
+
+		if strings.HasPrefix(weatherjson, "http/error/") {
+			return fmt.Sprintf("Weather Error: %s", weatherjson)
+		}
 
 		if len(weatherjson) > 0 {
 			err := json.Unmarshal([]byte(weatherjson), &openweathermapdata)
