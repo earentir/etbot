701e62457fe1905d20d1e6e48b05821c8de0eaf5
earentir <earentir@gmail.com>
2023-01-05 23:13:34 +0200

    need to plan for the cli args

--- cli.go
+++ cli.go
@@ -3,7 +3,7 @@ package main
 import cli "github.com/jawher/mow.cli"
 
 func cliTBStart(cmd *cli.Cmd) {
-	cmd.Spec = "[-s]"
+	cmd.Spec = "[-s] [-v] CONFIG"
 
 	TBSAllowOutput := cmd.BoolOpt("S", false, "Allow the Bot to output to twitch")
 	_ = TBSAllowOutput
